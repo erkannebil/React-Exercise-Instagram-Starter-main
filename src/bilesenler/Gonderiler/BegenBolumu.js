6da5305378ffc9361342d4e9410a6ece2e583067
Nebi Resul Erkan <80630016+erkannebil@users.noreply.github.com>
2024-02-09 00:40:05 +0300

    liked parts is done

--- src/bilesenler/Gonderiler/BegenBolumu.js
+++ src/bilesenler/Gonderiler/BegenBolumu.js
@@ -2,25 +2,39 @@
 // Doğru beğeni sayısını görüntülemek için proplardan gelen bir veri parçasını kullanın.
 // Beğeni sayısını artırmak için "gonderiyiBegen" fonksiyonunu kullanan bir onClick işleyicisi de ekleyeceksiniz.
 // (Ek görev olarak, kullanıcınızın aynı gönderiyi birden fazla kez "beğenmesini" engelleyin.)
-import React from "react";
+
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faComment, faHeart } from "@fortawesome/free-regular-svg-icons";
 
 const BegenBolumu = (props) => {
-  // 🔥 Bu bileşenin parentının aşağıdaki propları düzgün gönderdiğinden emin olun.
-  const { gonderiyiBegen, begeniSayisi } = props;
+  const { gonderiyiBegen, begeniSayisi: propBegeniSayisi } = props;
+  const [liked, setLiked] = useState(false);
+  const [begeniSayisi, setBegeniSayisi] = useState(propBegeniSayisi);
+
+  useEffect(() => {
+    setBegeniSayisi(propBegeniSayisi);
+  }, [propBegeniSayisi]);
+
+  const handleBegenClick = () => {
+    if (!liked) {
+      gonderiyiBegen();
+      setLiked(true);
+      setBegeniSayisi((oncekiSayi) => oncekiSayi + 1);
+    }
+  };
 
   return (
     <div>
       <div className="like-section" key="likes-icons-container">
         <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faHeart} />
+          <FontAwesomeIcon icon={faHeart} onClick={handleBegenClick} />
         </div>
         <div className="like-section-wrapper">
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className="like-number">100 likes</p>
+      <p className="like-number">{begeniSayisi}</p>
     </div>
   );
 };
