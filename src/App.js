e8ba1ef3c5f3fe06de56a8a8d9d1a21889de2507
Nebi Resul Erkan <80630016+erkannebil@users.noreply.github.com>
2024-02-08 17:26:07 +0300

    first commitd

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
-/* 
-  Buradan başlayın ve iç içe geçmiş bileşenlere doğru ilerleyin.
-  Projedeki tüm dosyalara kod eklenmesi gerekmez.
-  Nerelerde değişiklik yapmanız gerektiğini anlamak için her dosyayı inceleyin.
-*/
 
-// State hook u import edin
-import React from "react";
-
-// Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
-// sahteVeri'yi import edin
+import React, { useState } from 'react';
 import "./App.css";
+import gonderiler from './bilesenler/Gonderiler/Gonderiler.js';
+import aramaCubugu from './bilesenler/AramaCubugu/AramaCubugu.js' ;
+import sahteVeri from './sahte-veri' ;
 
 const App = () => {
-  // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
-  // Artık sahteVeri'ye ihtiyacınız olmayacak.
-  // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
-
+   let [gonderiler, setGonderiler] = useState(sahteVeri);
+   let [aramaKriteri, setAramaKriteri] = useState(" ");
   const gonderiyiBegen = (gonderiID) => {
-    /*
-      Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
+    setGonderiler(gonderiler.map(gonderi => gonderi.id === gonderiID ? {...gonderi, beğeniSayısı: gonderi.beğeniSayısı + 1} : gonderi));
 
-      Uygulamanın durumu, React ağacının en üstünde bulunur, ancak iç içe geçmiş bileşenlerin stateleri değiştirememesi adil olmaz!
-      Bu fonksiyon, belirli bir gönderinin beğeni sayısını artırılmasına olanak sağlamak amacıyla iç içe geçmiş bileşenlere aktarılır.
-
-	  "setGonderi" yi çağırın ve state ine "posts.map" çağrısını iletin.
-      `map` içine iletilen callback aşağıdaki mantığı gerçekleştirir:
-        - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
-        - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
-     */
   };
 
   return (
